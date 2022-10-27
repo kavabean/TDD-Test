ede4c6b5441cce1cf2312d6cfcfe94a70d48340d
kavabean <gdegruccio@gmail.com>
2022-10-26 20:22:03 -0400

    birthday

--- animal.py
+++ animal.py
@@ -1,1 +1,13 @@
-pass
+class Animal:
+    
+    def __init__(self, type, size, age, name):
+        type = ''
+        size = ''
+        age = ''
+        name = ''
+
+    def speak():
+        pass
+
+    def describe():
+        pass
