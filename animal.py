d01865ac688b06471a140b670d2ca00146acef4e
kavabean <gdegruccio@gmail.com>
2022-11-08 02:16:08 -0500

    part 3

--- animal.py
+++ animal.py
@@ -1,12 +1,53 @@
+
 class Animal:
-    def __init__(self):
-        self.type = ''
+    def __init__(self, type):
+        self.type = type
         self.size = ''
         self.age = 0
-        self.name = ''
+        self.name = self.get_name()
 
     def speak(self):
-        pass
+        print('here')
+        if self.size == 'small':
+            if self.type == 'cat':
+                result = 'meow'
+            elif self.type == 'dog':
+                result = 'bow wow'
+            else:
+                pass
+        elif self.size == 'medium':
+            if self.type == 'cat':
+                result = 'MEOW!'
+            elif self.type == 'dog':
+                result = 'Ruff ruff'
+            else:
+                pass
+        elif self.size != '':
+            if self.type == 'dog':
+                result = 'arf arf'
+        else:
+            result = 'New animal discovered'
+        
+        return(result)
 
     def describe(self):
-        pass
+
+        if self.age < 10:
+            return(f'{self.name} is young')
+        elif self.age >= 10:
+            return(f'{self.name} is old')
+
+
+    def get_name(self):
+        if self.type == 'cat':
+            return 'Seymour'
+        elif self.type == 'dog':
+            return 'Spot'
+
+    def animal_calib(self, type, name, age, size):
+        self.name = name
+        self.age = age
+        self.size = size
+        self.type = type
+
+        self.get_name()
--- tests/test_animal_class.py
+++ tests/test_animal_class.py
@@ -1,82 +1,155 @@
-from cgitb import small
 import unittest
+
 from animal import Animal
 
 class Test_animal(unittest.TestCase):
 
-    def test_name_cat(self):
-        checkcat = Animal('cat')
+    def test_default_name_cat(self):
+
+        #GIVEN - a new instance of Animal whose type is "cat"
+
+        cat = Animal("cat")
+
+        #WHEN - no action
 
         pass
 
-        self.assertEqual(checkcat.name, 'Seymour')
+        #THEN - the name property of cat is "Seymour"
 
-    def test_name_dog(self):
-        checkdog = Animal('dog')
-        
+        self.assertEqual(cat.name, "Seymour")
+
+    def test_default_name_dog(self):
+
+        #GIVEN - a new instance of Animal whose type is "dog"
+
+        dog = Animal("dog")
+
+        #WHEN - no action
+
         pass
-            
-        self.assertEqual(checkdog.name, 'Spot')
 
-    def test_animal_name(self):
-        checkname = Animal()
-        name = Animal(name)
-        
+        #THEN - the name property of cat is "Seymour"
+
+        self.assertEqual(dog.name, "Spot")
+
+    def test_provided_name(self):
+
+        #GIVEN - a new instance of Animal whose type is anything, in this case "dog". And whose name is assigned explicitly.
+
+        assigned_name = "Killer"
+
+        dog = Animal("dog")
+
+        dog.name = assigned_name
+
+        #WHEN - no action
+
         pass
 
-        self.assertEqual(checkname.name, name)
+        #THEN - the name property is the assigned name
 
-    def test_animal_cat_size(self):
-        checksize1 = Animal('cat')
-        checksize1.size = 'small'
+        self.assertEqual(dog.name, assigned_name)
 
-        self.speak1 = checksize1.speak()
+    def test_cat_speak(self):
 
-        self.assertEqual(self.speak1, 'meow')
+        #GIVEN - a new instance of Animal whose type is "cat" and size is 'small'
 
+        cat1 = Animal("cat")
 
-        checksize2 = Animal('cat')
-        checksize2.size = 'small'
+        cat1.size = "small"
 
-        self.speak2 = Animal.speak()
+        #WHEN - we call speak()
 
-        self.assertEqual(self.speak2, 'meow')
+        words1 = cat1.speak()
 
-    def test_animal_dog_size(self):
-        checksize1 = Animal('dog')
-        checksize1.size = 'small'
+        #THEN - return value = "meow"
 
-        self.speak1 = checksize1.speak()
+        self.assertEqual(words1, "meow")
 
-        self.assertEqual(self.speak1, 'bow wow')
+        #GIVEN - a new instance of Animal whose type is "cat" and size is NOT 'small'
 
+        cat2 = Animal("cat")
 
-        checksize2 = Animal('dog')
-        checksize2.size = 'medium'
+        cat2.size = "medium"
 
-        self.speak2 = Animal.speak()
+        #WHEN - we call speak()
 
-        self.assertEqual(self.speak2, 'Ruff ruff')
+        words2 = cat2.speak()
 
+        #THEN - return value = "MEOW!"
 
-        checksize3 = Animal('dog')
-        checksize3.size = not 'medium' or 'small'
+        self.assertEqual(words2, "MEOW!")
 
-        self.speak3 = Animal.speak()
+    def test_dog_speak(self):
 
-        self.assertEqual(self.speak3, 'arf arf')
+        #GIVEN - a new instance of Animal whose type is "dog" and size is 'small'
 
-    def test_animal_age(self):
-        checkage1 = Animal(self.age)
-        checkage1 < 10
-        
-        self.age1 = Animal.describe()
+        dog1 = Animal("dog")
 
-        self.assertEqual(self.age1, f'{checkage1} is young')
+        dog1.size = "small"
 
-        checkage2 = Animal(self.age)
-        checkage2 >= 10
-        
-        self.age2 = Animal.describe()
+        #WHEN - we call speak()
 
-        self.assertEqual(self.age2, f'{checkage2} is old')
+        words1 = dog1.speak()
+
+        #THEN - return value = "bow wow"
+
+        self.assertEqual(words1, "bow wow")
+
+        #GIVEN - a new instance of Animal whose type is "dog" and size is 'medium'
+
+        dog2 = Animal("dog")
+
+        dog2.size = "medium"
+
+        #WHEN - we call speak()
+
+        words2 = dog2.speak()
+
+        #THEN - return value = "Ruff ruff"
+
+        self.assertEqual(words2, "Ruff ruff")
+
+        #GIVEN - a new instance of Animal whose type is "dog" and size is not 'small' or 'medium'
+
+        dog3 = Animal("dog")
+
+        dog3.size = "notsmallormedium"
+
+        #WHEN - we call speak()
+
+        words3 = dog3.speak()
+
+        #THEN - return value = "arf arf"
+
+        self.assertEqual(words3, "arf arf")
+
+    def test_age(self):
+
+        #GIVEN - a new instance of Animal whose type is anything, in this case "dog" and whose age is less than 10 (test with '9')
+
+        dog1 = Animal("dog")
+
+        dog1.age = 9
+
+        #WHEN - we call describe()
+
+        desc1 = dog1.describe()
+
+        #THEN - return "[name] is young"
+
+        self.assertEqual(desc1, f"{dog1.name} is young")
+
+        #GIVEN - a new instance of Animal whose type is anything, in this case "dog" and whose age is NOT less than 10 (test with '10')
+
+        dog2 = Animal("dog")
+
+        dog2.age = 10
+
+        #WHEN - we call describe()
+
+        desc2 = dog2.describe()
+
+        #THEN - return "[name] is young"
+
+        self.assertEqual(desc2, f"{dog2.name} is old")
